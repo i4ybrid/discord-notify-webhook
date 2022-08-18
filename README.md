4bd3c773cb537383b1eb00cbb95fcef62c3e2552
Eric Hu <cryogenicsleep@hotmail.com>
2022-08-18 13:49:40 -0400

    Added logic to fetch guild info based on your discord token, and to figure out linking to channels

--- README.md
+++ README.md
@@ -10,21 +10,27 @@ cd discord-notify-webhook
 npm install
 ```
 
-
 ## Usage
 
 First of all, this only works on Linux variants that use dbus, and on Google Chrome right now. If you want more support for another OS or browser, please let me know.
 
 * Setup a webhook in a Discord channel of your choosing to receive your notifications. YOU MUST TURN OFF NOTIFICATIONS in this channel. Otherwise you could be running in an infinite loop. You could also mute it for safe measure, but I haven't.
-* Copy the .env_sample to .env locally, and fill in your webhook. The Discord token is not needed right now, but possibly in a future release, this will be needed for extra features.
+* Copy the .env_sample to .env locally, and fill in your webhook. The token is used to fetch all your guild information and try to generate a link to a the correct channel
 * Run npm install on the root directory. This was tested with node version 14.17.0, so if there are compatibility issues, you can revert to that version.
 * Open Chrome, log into Discord, and turn on your desktop notifications in the settings. Make sure you can get a desktop notification.
 * Run node `sendWebhookOnNotify.js`. Optionally you can spit the console out into a log useing `sendWebhookOnNotify.js > webhookNotify.log`
+
+# Optional
+* Set your token in the .env file, this is not needed
+* Enter discord channels in config/priorityGuilds.txt
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
+
+## Open Bugs
+* The application will try to match the right channel and category as best as possible, but the channel is ambiguous, so it can get this wrong. There is no fix at this time
 
 ## License
 [ISC](https://opensource.org/licenses/ISC)
--- sendWebhookOnNotify.js
+++ sendWebhookOnNotify.js
@@ -1,8 +1,18 @@
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
 const { spawn } = require('child_process');
+const fetch = require('node-fetch');
+const fs = require('fs');
 require('dotenv').config();
 
 let hook;
+
+const dupePrevention = new Set();
+const channelToId = {}; //map of { `${categoryName} | #${channelName}` : `${guildId}/${channelId}` }
+const priorityGuilds = new Set();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 /**
  * Will process message, returns null if filtered or an error happens
@@ -23,7 +33,7 @@ function processString(inputString) {
         const resultData = getSenderInfoFromString(lines[4]);
         const messageData = parseMessages(lines, 6);
         if (resultData) {
-          return {...resultData, message: messageData};
+          return { ...resultData, message: messageData };
         }
       }
     }
@@ -52,15 +62,15 @@ function parseMessages(lines, startIndex) {
       if (matchResults && matchResults[0]) { //If the string has ended, finish up
         keepProcessing = false;
         if (resultString.endsWith('"')) { //Remove trailing "
-          resultString = resultString.substring(0, resultString.length-1);
+          resultString = resultString.substring(0, resultString.length - 1);
         }
       } else if (s.length > 0) { //Otherwise keep appending
-        
+
         resultString = resultString.length > 0 ? `${resultString}\n${s}` : s;
       }
     }
     lineIndex++;
-    
+
     if (keepProcessing !== true || lineIndex >= lines.length) { //Terminating loop condition
       return resultString;
     }
@@ -76,32 +86,47 @@ function parseMessages(lines, startIndex) {
  */
 function getSenderInfoFromString(senderLine) {
   //
-  const justData = senderLine.replace(/(^ *string ")|("$)/g,'');
+  const justData = senderLine.replace(/(^ *string ")|("$)/g, '');
   const matchResultsOfName = justData.match(/ \(#.+\)$/);
   if (matchResultsOfName && matchResultsOfName.length > 0 && matchResultsOfName.index > 0 && matchResultsOfName[0]?.length > 3) {
-    const [channelName, categoryName] = matchResultsOfName[0].substring(2, matchResultsOfName[0].length-1).split(', ');
+    const [channelName, categoryName] = matchResultsOfName[0].substring(2, matchResultsOfName[0].length - 1).split(', ');
     const senderName = justData.substring(0, matchResultsOfName.index);
-    return {senderName, channelName, categoryName};
+    return { senderName, channelName, categoryName };
   } else {
     return null;
   }
 }
 
+function checkUnique(payload) {
+  const payloadString = JSON.stringify(payload);
+  if (!dupePrevention.has(payloadString)) {
+    dupePrevention.add(payloadString);
+    setTimeout(() => {
+      dupePrevention.delete(payloadString);
+    }, 5000);
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function sendWebhook(payload) {
+  if (!checkUnique(payload)) {
+    return;
+  }
+
   //Not using destructuring to allow nulls
   const senderName = payload.senderName;
   const channelName = payload.channelName;
   const categoryName = payload.categoryName;
   const message = payload.message;
+  const channelUrlSuffix = channelToId[`${categoryName} | ${channelName}`];
   let channelHyperlink;
 
-  if (global.channelMap?.[categoryName]?.[channelName]) {
-    const channelInfo = global.channelMap[categoryName]?.[channelName];
-    const guildId = channelInfo.guild_id;
-    const channelId = channelInfo.id;
-    if (guildId && channelId) {
-      channelHyperlink = `[${channelName}](https://discord.com/channels/${guildId}/${channelId})`;
-    }
+  if (channelUrlSuffix) {
+    channelHyperlink = `[${channelName}](https://discord.com/channels/${channelUrlSuffix})`;
+  } else {
+    console.log(`Couldn't find the channel for [${categoryName} | ${channelName}]`);
   }
 
   const embed = new MessageBuilder()
@@ -118,6 +143,16 @@ function sendWebhook(payload) {
 }
 
 function initialize() {
+  try {
+    const priorityGuildsText = fs.readFileSync('./config/priorityGuilds.txt', 'utf8');
+    const priorityGuildArray = priorityGuildsText.split(/(\r\n)+/g);
+    priorityGuildArray.forEach((priorityGuild) => {
+      priorityGuilds.add(priorityGuild);
+    });
+  } catch (e) {
+    //no-op
+  }
+
   const webhookUrl = process.env.NOTIFICATION_WEBHOOK;
   if (!webhookUrl) {
     console.error('You need to set a discord webhook in an .env file. Use .env_sample as an example of what to do, just put your webhook after the NOTIFICATION_WEBHOOK=');
@@ -127,18 +162,75 @@ function initialize() {
 
   const discordToken = process.env.DISCORD_TOKEN;
   if (discordToken) {
-    //TODO Gather all messages and load data into a guild map
+    loadChannelMap(discordToken);
   }
 }
 
-function loadChannelMap(discordToken) {
-
+function guildSorter(a, b) {
+  if (priorityGuilds.has(a.id) || priorityGuilds.has(a.name)) {
+    return -1;
+  } else if (priorityGuilds.has(b.id) || priorityGuilds.has(b.name)) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-(() => {
-  initialize();
-  //Adding the sender because I was getting duplicate notifications, there may be a better fix for this
-  const command = spawn('dbus-monitor', ["interface='org.freedesktop.Notifications',sender=':1.50',member='Notify'"]);
+/**
+ * Fetches all channel data periodically (rate limited), and then put it into the channelToId map.
+ * Tuples in object are: [ `${categoryName} | #${channelName}`, `${guildId}/${channelId}` ]
+ * 
+ * @param {string} discordToken
+ */
+async function loadChannelMap(discordToken) {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: discordToken,
+  };
+
+  const allGuilds = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+    method: 'GET',
+    headers
+  }).then((res) => res.json())
+    .then((json) => json) || [];
+
+  const sortedGuilds = allGuilds.sort(guildSorter);
+  
+  const guildFetchPromise = sortedGuilds.reduce((promiseAccumulator, guildInfo) => {
+    return promiseAccumulator.then(async () => {
+      const fetchedChannels = await fetch(`https://discord.com/api/v10/guilds/${guildInfo.id}/channels`, {
+        method: 'GET',
+        headers
+      }).then((res) => res.json())
+        .then((json) => json);
+
+      const fetchedCategories = await fetchedChannels.filter((guildInfo) => {
+        return 4 === guildInfo?.type;
+      });
+      const fetchedTextChannels = await fetchedChannels.filter((guildInfo) => {
+        return  0 === guildInfo?.type;
+      });
+
+      const categoryNameMap = await new Map(fetchedCategories.map((category) => {
+        return [category.id, category.name];
+      }));
+      const textNameMap = await new Map(fetchedTextChannels.map((channel) => {
+        const categoryName = categoryNameMap.get(channel.parent_id) || '';
+        channelToId[`${categoryName} | #${channel.name}`] = `${guildInfo.id}/${channel.id}`;
+        //console.log(`${categoryName} | #${channel.name} = ${guildInfo.id}/${channel.id}`);
+        return [`${categoryName} | #${channel.name}`, `${guildInfo.id}/${channel.id}`];
+      }));
+
+      console.log(`loadChannelMap Processed [${guildInfo.name}] /${guildInfo.id}; Fetched ${fetchedCategories.length} categories and ${fetchedTextChannels.length} text channels. Processed ${textNameMap.size} text channels`);
+
+      await sleep(5000);
+    });
+  }, Promise.resolve());
+}
+
+(async () => {
+  await initialize();
+  const command = spawn('dbus-monitor', ["interface='org.freedesktop.Notifications',member='Notify'"]);
 
   command.stdout.on('data', (output) => {
     const payload = processString(output.toString());
